0028bf74c8d0af19ce7475bb1024e9bc83c43e58
Muhammad Minhaj Uddin <minhajchamp@gmail.com>
2020-02-08 01:51:09 +0500

    Add files via upload
    initial (commit)

---
+++ index.php
@@ -0,0 +1,235 @@
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Chris Dermody's GitHub Projects</title>
+
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+		<link rel='stylesheet' href='css/style.css' >
+		<link rel="stylesheet" href="css/animate.css">
+		
+
+
+	</head>
+	<body>
+
+
+		<div class="container-fluid" style='margin-top:50px;'>
+
+			<div class="text-center">
+				<img src="https://avatars2.githubusercontent.com/u/23106383?s=460&v=4" width=200 class="profile-pic">
+				<h1>Minhaj Uddin</h1>
+			  <h3>Full Stack Development with a sprinkle of SEO</h3>
+        <p>Computer Science graduate, self and avid learner. </p>
+        <span><b>Current learning -> </b>Deploying apps on AWS, CI/CD pipeling.</span>
+			  <div class="text-center" style="margin:20px">
+			  	<a href="http://chrisdermody.com/" class="" target="_blank">
+			  	  minhajuddin.github.io
+			  	</a>
+			  </div>
+			</div>
+
+			<div class="row row-grey">
+			    <div class="col-sm-8 col-sm-offset-2">
+
+			    	<div class="row work-item-row">
+			    		<div class="col-lg-8">
+			    			<img src="https://i.ibb.co/51LY0mB/FULL-UNITED-imac2013-front.png" width='350' style='display:inline-block;' class="img-responsive center"> 			<img src="https://i.ibb.co/RHSzcYF/phone-nexus5x-portrait.png" width='250' class="img-responsive center" style='display:inline-block;'>
+                
+			    		</div>
+			    		<div class="col-lg-4">
+			    			<h2><a href="https://mydevportfol.io?ref=ghportfolio" target="blank">United Stitched</a></h2>
+			    			<p></p>
+			    			<h4>Graphics</h4>
+			    			<p>United Stitched is a graphic designing firm that assist customers in several platforms of embroidery digitizing, vector art services and patch designing.</p>
+                
+                <h4>Technology</h4>
+                <p>United Stitched is mobile responsive with a proper and interactive UI. It also contains loading capacity with a CSS3 developed loader.
+			    		</div>
+		    		</div>
+		    		<div class="row text-center">
+		    			<div class="col-xs-12">
+		    				<img src="https://i.ibb.co/KLcXvcd/vector-javascript-1.png" class="techlogo" width="100">
+		    				<img src="https://i.ibb.co/R7NJWDQ/jquery-8-1175153.png" class="techlogo" width="100">
+		    				<img src="https://i.ibb.co/bQt5ZQ1/73835fa38fba6d35aff9de603dc5044a-php-programming-language-icon-by-vexels.png" class="techlogo" width="100">
+		    				<img src="" class="techlogo" width="100">
+		    				<img src="" class="techlogo" width="100">
+		    				<img src="" class="techlogo" width="100">
+		    			</div>
+		    		</div>
+		    		<div class="row text-center" style="margin:20px;">
+		    				<a class="btn btn-launch" href="https://unitedstitched.com" target="blank">Visit</a>
+	    			</div>
+					</div>
+			</div>
+
+
+			<div class="row row-white">
+			    <div class="col-sm-8 col-sm-offset-2">
+
+			    	<div class="row work-item-row">
+			    		<div class="col-lg-8 col-lg-push-4">
+			    			<img src="https://i.ibb.co/98zv57d/nexus5x-portrait.png" class="img-responsive center" style='display:inline-block;' width="300">
+                <img src="https://i.ibb.co/M54xW31/ssf-imac2013-front.png" class="img-responsive center" style='display:inline;' width="300">
+			    		</div>
+			    		<div class="col-lg-4 col-lg-pull-8">
+			    			<h2><a href="http://jobb.ie" target="blank">Stitch Stack</a></h2>
+			    			<p>Stitch Stack is embroidery digitizing company. The company provides digitizing services alongisde with Vector products. They also provide real sew outs printed on Tajima machines.</p>
+			    			<h4>Technology</h4>
+			    			<p>Parallex design, animate.css, and many more stuff. Used JQuery for minor animation. The backend is built using Core PHP with database support of MySQL. Also used JSChart to produce charts. Ajax is also used.</p>
+			    		</div>
+		    		</div>
+		    		<div class="row text-center">
+		    			<div class="col-xs-12">
+                
+                <img src="https://i.ibb.co/KLcXvcd/vector-javascript-1.png" class="techlogo" width="100">
+		    				<img src="https://i.ibb.co/R7NJWDQ/jquery-8-1175153.png" class="techlogo" width="100">
+		    				<img src="https://i.ibb.co/bQt5ZQ1/73835fa38fba6d35aff9de603dc5044a-php-programming-language-icon-by-vexels.png" class="techlogo" width="100">
+                
+		    			</div>
+		    		</div>
+		    		<div class="row text-center" style="margin:20px;">
+		    				<a class="btn btn-launch" href="http://jobb.ie" target="blank">Launch</a>
+	    			</div>
+					</div>
+			</div>
+
+
+			<div class="row row-grey">
+			    <div class="col-sm-8 col-sm-offset-2">
+
+			    	<div class="row work-item-row">
+			    		<div class="col-lg-8">
+			    			<img src="https://i.ibb.co/dL2Q02q/imac2015-front.png" class="img-responsive center-block" width="350">
+			    		</div>
+			    		<div class="col-lg-4">
+			    			<h2><a href="https://play.google.com/store/apps/details?id=com.chrisdermody.mydeliverypal&hl=en" target="blank">Random Meal Generator</a></h2>
+			    			<p>Just a practice project. But learnt a lot from it. Used open-source API that gets you random meal from across the globe. </p>
+			    			<h4>Technology</h4>
+			    		<p>Parsing JSON encoded using JSON.parse. Pure Javscript is used. A little bit of JQuery is also used to uplift the stuff. Font awesome icons used.</p>
+			    		</div>
+		    		</div>
+		    		<div class="row text-center">
+		    			<div class="col-xs-12">
+		    				<img src="https://i.ibb.co/KLcXvcd/vector-javascript-1.png" class="techlogo" width="100">
+		    				<img src="https://i.ibb.co/R7NJWDQ/jquery-8-1175153.png" class="techlogo" width="100">
+		    				<img src="https://i.ibb.co/bQt5ZQ1/73835fa38fba6d35aff9de603dc5044a-php-programming-language-icon-by-vexels.png" class="techlogo" width="100">
+		    			</div>
+		    		</div>
+		    		<div class="row text-center" style="margin:20px;">
+		    				<a class="btn btn-launch" href="https://play.google.com/store/apps/details?id=com.chrisdermody.mydeliverypal&hl=en" target="blank">Go to app</a>
+	    			</div>
+					</div>
+			</div>
+
+
+
+			<div class="row row-white">
+			    <div class="col-sm-8 col-sm-offset-2">
+			    	<div class="row work-item-row">
+			    		<div class="col-lg-8 col-lg-push-4">
+			    			<img src="img/vistoryscreen1.png" class="img-responsive center-block">
+			    		</div>
+			    		<div class="col-lg-4 col-lg-pull-8">
+			    			<h2><a href="http://vistoryapp.com" target="blank">Vistory</a></h2>
+			    			<h4>Mobile Car App</h4>
+			    			<p>An Android application for vehicle owners to quickly and easily log maintenance and parts costs for their cars.</p>
+			    			<h4>Technology</h4>
+			    			<p>Parsing JSON encoded using JSON.parse. Pure Javscript is used. A little bit of JQuery is also used to uplift the stuff. Font awesome icons used.</p>
+			    		</div>
+			    	</div>
+		    		<div class="row text-center">
+		    			<div class="col-xs-12">
+		    				<img src="img/techlogos/ionic.png" class="techlogo" width="100">
+		    				<img src="img/techlogos/angular.png" class="techlogo" width="100">
+		    				<img src="img/techlogos/node.png" class="techlogo" width="100">
+		    				<img src="img/techlogos/heroku.png" class="techlogo" width="100">
+		    				<img src="img/techlogos/pouch-grey.png" class="techlogo" width="100">
+		    				<img src="img/techlogos/cloudant.png" class="techlogo" width="100">
+		    			</div>
+		    		</div>
+		    		<div class="row text-center" style="margin:20px;">
+		    				<a class="btn btn-launch" href="http://vistoryapp.com" target="blank">Open app site</a>
+	    			</div>
+					</div>
+			</div>
+
+			<div class="row row-grey">
+			    <div class="col-sm-8 col-sm-offset-2">
+			    	<div class="row work-item-row">
+			    		<div class="col-lg-8 col-lg-push-4 text-center">
+			    			<img src="img/techlogos/Slack.png" class="img-responsive center-block" width="300" style="margin: auto; margin-top:50px">
+			    		</div>
+			    		<div class="col-lg-4 col-lg-pull-8">
+			    			<h2><a href="https://github.com/Chippd/slackDomainChecker" target="blank">Slack Chat Bot</a></h2>
+			    			<p>A Slack chat bot built to quickly check domain name availability across the most popular TLD's. </p>
+			    			<h4>Technology</h4>
+			    			<p>The Slack bot interacts with a Node.js app living on Heroku, which requests information from a free domain-checking api, parses the response, and delivers a colour-coded response back to the client.</p>
+			    		</div>
+			    	</div>
+		    		<div class="row text-center">
+		    			<div class="col-xs-12">
+		    				<img src="img/techlogos/node.png" class="techlogo" width="100">
+		    				<img src="img/techlogos/heroku.png" class="techlogo" width="100">
+		    			</div>
+		    		</div>
+		    		<div class="row text-center" style="margin:20px;">
+		    				<a class="btn btn-launch" href="https://github.com/Chippd/slackDomainChecker" target="blank">View on Github</a>
+	    			</div>
+					</div>
+			</div>
+
+			<div class="row row-tools row-white">
+			    <div class="col-sm-12 text-center">
+
+			    	<h3>Tools and Technologies I Use</h3>
+    				<img src="https://i.ibb.co/qCgMKy6/1200px-Visual-Studio-Code-1-35-icon-svg.png" class="techlogo" width="100">
+    				<img src="https://i.ibb.co/ZgyjFXj/684879.png" class="techlogo" width="100">
+    				<img src="https://i.ibb.co/p4r3YtM/Git-icon-svg.png" class="techlogo" width="100">
+<img src="https://i.ibb.co/cYbWMMW/xampp-logo-4db0ac715559be7e8da62bf9b8de42aad71d1b8ab32f18cd14b4505ac7812a7f.png" class="techlogo" width="100">
+					</div>
+			</div>
+
+			<div class="row row-footer text-center">
+				<div class="col">
+  				<div style="margin:50px;">
+  					<h3 style="margin:50px 0 15px">I'm available for work, get in touch</h3>
+				  	<a href="http://chrisdermody.com/" class="" target="_blank">
+              minhajuddin.github.io
+            </a>
+				  </div>
+				</div>
+			</div>
+
+
+
+
+		</div>
+<!--
+		<div class="container-fluid">
+
+
+
+		</div> -->
+
+<!-- 		<div class="text-center">
+				<p style="margin-top:20px"">*Also love building other things, like VBA macros, mobile applications, chat bots, and the odd motorbike...</p>
+			</div> -->
+
+
+
+		<!-- Including Bootstrap JS (with its jQuery dependency) so that dynamic components work -->
+		<script src="js/jquery-3.1.0.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<!-- <script src="js/myScripts.js"></script> -->
+
+	</body>
+</html>
+
